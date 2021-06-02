4ffb770555ae29e87f5f36c9e525f41570a2b2c8
“Tony <adrprogramming@gmail.com>
2021-06-02 16:32:33 -0600

    fixed green cards

--- fit-club/src/components/ExerciseCard.js
+++ fit-club/src/components/ExerciseCard.js
@@ -1,41 +1,57 @@
+import { render } from '@testing-library/react'
 import React from 'react'
 
-function ExerciseCard(props) {
+class ExerciseCard extends React.Component {
 
-    const handleWorkout = () => {
-        if (props.addWorkout) {
-            props.addWorkout(props.exercise)
+    state = {
+        click: false
+    }
+
+    handleWorkout = () => {
+        if (this.props.addWorkout) {
+            this.props.addWorkout(this.props.exercise)
         }
     }
-    
-function readMore() {
-    console.log('clicked')
-}
-    return (
-        <div className='card' onClick={handleWorkout}>
-            <div className='card-inner'>
-                <div className='card-front'>
-                    <div className='card-content'>
-                        <h2 className='weight'>💪🏼</h2>
-                        <h2 className='exercise-name'>{props.exercise.name}</h2>
-                        <h4 className='muscle'>{props.exercise.muscleGroup}</h4>
-                        <h4 className='equipment'>{props.exercise.equpipmentNeeded}</h4>
-                        <button className='flip-button' onClick={readMore}>Read More</button>
-                    </div>
-                </div>
-                {/* <div className='card'>
-                    <div className='card-back'>
-                        <div className='card-back-content'>
-                            <h2 className='exercise-name'>{props.exercise.name}</h2>
-                            <p className='workout-details'>{props.exercise.workoutDescription}</p>
-                            <h4 className='muscle'>{props.exercise.muscleGroup}</h4>
-                            <h4 className='equipment'>{props.exercise.equpipmentNeeded}</h4>
+
+    readMore = (e) => {
+        e.stopPropagation()
+        this.setState({
+            click: !this.state.click
+        })
+    }
+    render() {
+        return (
+            <div>
+                {!this.state.click
+
+                    ?
+
+                    <div className='card-front' onClick={this.handleWorkout}>
+                        <div className='card-content'>
+                            <h2 className='weight'>💪🏼</h2>
+                            <h2 className='exercise-name'>{this.props.exercise.name}</h2>
+                            <h4 className='muscle'>{this.props.exercise.muscleGroup}</h4>
+                            <h4 className='equipment'>{this.props.exercise.equpipmentNeeded}</h4>
+                            <button className='flip-button' onClick={this.readMore}>Read More</button>
                         </div>
                     </div>
-                </div> */}
+
+                    :
+
+                    <div className='card-back' onClick={this.handleWorkout}>
+                        <div className='card-back-content'>
+                            <h2 className='exercise-name'>{this.props.exercise.name}</h2>
+                            <p className='workout-details'>{this.props.exercise.workoutDescription}</p>
+                            <h4 className='muscle'>{this.props.exercise.muscleGroup}</h4>
+                            <h4 className='equipment'>{this.props.exercise.equpipmentNeeded}</h4>
+                            <button className='flip-button' onClick={this.readMore}>Go Back</button>
+                        </div>
+                    </div>
+                }
             </div>
-        </div>
-    )
+        )
+
+    }
 }
 
 export default ExerciseCard
